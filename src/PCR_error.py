e8b43a253098602ed1295861cdd538fecc7f81c9
fidan <f.rabiafidan@gmail.com>
2021-12-03 08:30:16 +0000

    PCR error addition - needs some debugging

--- src/PCR_error.py
+++ src/PCR_error.py
@@ -1,13 +1,11 @@
 import os
 import numpy as np
 import random
-from Bio import SeqIO,pairwise2
-from Bio.Seq import Seq
-from Bio.pairwise2 import format_alignment
+from Bio import SeqIO
 import pandas as pd
-import glob
-import sys
-from collections import Counter
+import subprocess
+from io import StringIO
+import re
 
 
 os.chdir("/nfs/research/goldman/rabia/wastewater/sars-cov-2-metagenomic-simulator/src")
@@ -65,137 +63,123 @@ def amplicon_lookup(PRIMER_BED,position):
       )
     corresponding_amplicons=list(df.loc[(df["Start_x"]<=position)&(position<=df["End_y"]),"Amp_no"])
     return corresponding_amplicons
- 
-def choose_reads_to_mutate(df_row):
-    reads=[]
-    for amp in df_row['amplicons']:
-        amp # type: str
-        to_mut=df_row.amplicon_source_genome_read_count_to_mutate[amp]
-        for genome,n_mut in to_mut.items():
-            if n_mut == 0 or df_row.amplicon_source_genome_read_count[amp][genome]==0:
-                pass
-            else:
-                indices=random.sample(list(range(df_row.amplicon_source_genome_read_count[amp][genome]+1)),
-                                    k=n_mut)
 
-                indices=[(x+1)*2 for x in indices] #read names are 1 based and even numbers
-                for idx in indices:
-                    reads.append(f'{genome}_amplicon_{amp}-{idx}/1')
-                    reads.append(f'{genome}_amplicon_{amp}-{idx}/2')
-    return reads
 
-amp_n_reads=pd.read_csv("../simulation_output/example_amplicon_abundances_summary.tsv",sep="\t")
-del amp_n_reads['Unnamed: 0']
-amp_n_reads['amplicon_number']=amp_n_reads['amplicon_number'].apply(str)
-amp_n_reads_grouped=amp_n_reads.groupby(['amplicon_number']).sum()
-amp_n_reads_grouped=amp_n_reads_grouped['n_reads']
-
-errors=pd.DataFrame(dict(errortype=["SUBS"]*SUBS_COUNT + ["INS"]*INS_COUNT + ["DEL"]*DEL_COUNT))
-errors['mut_index']=errors.index
-errors["length"]=[1]*SUBS_COUNT + random.choices(list(range(1,INS_MAX_LENGTH+1)),k=INS_COUNT) + list(np.random.geometric(p=DEL_LENGTH_GEOMETRIC_PARAMETER, size=DEL_COUNT))
+errors=pd.DataFrame(dict(errortype=["SUBS"]*SUBS_COUNT + ["DEL"]*DEL_COUNT + ["INS"]*INS_COUNT ))
+errors["mut_indices"]=errors.index
+errors["length"]=[1]*SUBS_COUNT + list(np.random.geometric(p=DEL_LENGTH_GEOMETRIC_PARAMETER, size=DEL_COUNT)) + random.choices(list(range(1,INS_MAX_LENGTH+1)),k=INS_COUNT) 
 errors["pos"]= random.sample(list(range(len(REF.seq))),k=SUBS_COUNT+INS_COUNT+DEL_COUNT)
 errors["ref"]=errors.apply(lambda x: REF.seq[x.pos], axis=1)
 errors["alt"]=errors.apply(lambda x: alts(x.ref,x.errortype,x.length), axis=1)
 errors["VAF"]=errors.apply(lambda x: np.random.dirichlet(VAF[x.errortype], size=None)[1],axis=1)
 errors["amplicons"]=errors.apply(lambda x: amplicon_lookup(PRIMER_BED,x.pos),axis=1)
-errors["amplicon_total_reads"]=errors.apply(lambda x:{amp:int(amp_n_reads_grouped[amp]) if amp in amp_n_reads_grouped.index else 0 for amp in x.amplicons} ,axis=1)
-errors["read_count_to_mutate_float"]=errors.apply(lambda x: {amp:int(amp_n_reads_grouped[amp]) * x.VAF if amp in amp_n_reads_grouped.index else 0 for amp in x.amplicons},axis=1)
-errors["read_count_to_mutate"]=errors.apply(lambda x: {amp:round(int(amp_n_reads_grouped[amp]) * x.VAF) if amp in amp_n_reads_grouped.index else 0 for amp in x.amplicons},axis=1)
 errors = errors.loc[errors['VAF']!=0,]
-errors["amplicon_source_genome_read_count"]=errors.apply(lambda x:{amp_of_genome:dict(amp_n_reads.loc[amp_n_reads['amplicon_number']==amp_of_genome,["ref","n_reads"]].to_records(index=False)) if amp_of_genome in list(amp_n_reads['amplicon_number']) else {} for amp_of_genome in x.amplicons } ,axis=1)
-errors["amplicon_source_genome_read_count_to_mutate"]=errors.apply(lambda x:{amp_of_genome:Counter(random.sample(list(amp_n_reads.loc[amp_n_reads['amplicon_number']==amp_of_genome,"ref"]),counts=list(amp_n_reads.loc[amp_n_reads['amplicon_number']==amp_of_genome,"n_reads"]),k=x.read_count_to_mutate[amp_of_genome])) if amp_of_genome in list(amp_n_reads['amplicon_number']) else {} for amp_of_genome in x.amplicons } ,axis=1)
-errors["amplicon_read_to_mutate"]=errors.apply(lambda x:choose_reads_to_mutate(x),axis=1)
 
 
-reads=[]
-for i in errors['amplicon_read_to_mutate'].tolist():
-    reads.extend(i)
+error_amplicons=[]
+for i in errors['amplicons'].tolist():
+    error_amplicons.extend(i)
 
-
-indices=[[errors["mut_index"][a]]*2*sum(errors.loc[a,"read_count_to_mutate"].values()) for a in range(errors.shape[0])]
+indices=[[errors.index[a]]*len(errors.loc[a,"amplicons"]) for a in range(errors.shape[0])]
 indices2=[]
 for i in indices:
     indices2.extend(i)
 
-read_df=pd.DataFrame(zip(reads,indices2),columns=["read","mut_index"])
-read_df2=read_df.groupby("read").sum()
 
-r1_list=list()
-for all_r1 in SeqIO.parse("tmp.sms.all_files_unshuffled1.fastq", format="fastq"):
-    if all_r1.id in reads:
-        seq_list=list(all_r1.seq)
-        qual_list=all_r1.letter_annotations['phred_quality']
-        muts=read_df.loc[read_df["read"]==all_r1.id,"mut_index"].tolist()
-        align=pairwise2.align.localms(REF.seq, all_r1.seq,5, -1, -3, -0.5)
-        print(format_alignment(*align[0],full_sequences=True))
+amplicons=[]
+n_reads=[]
 
-    else:
-        pass#write back to the new file
+for i in df_amplicons.itertuples():
+    
+    mut_indices=[indices2[idx] for idx,a in enumerate(error_amplicons) if a==str(i.amplicon_number)]
+    amp=i.amplicon_filepath.replace("&","\\&").replace("|","\\|")
+    
+    alignment = subprocess.run(
+        ["bowtie2", 
+        "-x", "../../data/ref/MN908947.3", 
+        "-f",f"../example/amplicons/{amp}"], capture_output=True)
 
-all_r1.id
+    alignment = StringIO(alignment.stdout.decode("UTF-8"))
+    
+    # read alignment data as a dataframe
+    df = pd.read_csv(alignment, sep="\t", skiprows=[0, 1, 2], header=None, names=[i for i in range(19)])
+    df = pd.DataFrame(df[[0,3,5,9]])
+    df = df.rename(columns={0:"name", 3:"start", 5:"CIGAR", 9: "seq"})
+    
+    CIGAR=""
+    for idx,cigar in enumerate(re.split("(M|D|I)", df.CIGAR[0])[:-1]):
+        if idx%2==0:
+            prev=cigar[:]
+        else:
+            CIGAR+= cigar*int(prev)
 
+    start_p=df.start[0]-1
+    seq=df.seq[0]
 
+    reads_df=pd.DataFrame()
+    seq_pos=[]
+    for mut_idx in mut_indices:
+        aim=errors.loc[mut_idx,"pos"]
+        seq_idx=0
+        ref_idx=start_p
+        for c in CIGAR:
+            if ref_idx==aim:
+                seq_pos.append(seq_idx)
+                break
+            if c=="M":
+                seq_idx += 1
+                ref_idx +=1
+            elif c=="D":
+                ref_idx+=1
+            elif c=="I":
+                seq_idx+=1
 
-for idx,err in enumerate(errors.amplicons):
-    for j in err:
-        print(glob.glob(f"*amplicon_{j}.1.fq"))
-    break
+    
+        mut_reads=int(i.n_reads * errors.loc[mut_idx,"VAF"])
+        reads=random.sample(range(i.n_reads),k=mut_reads)
+        reads=[str(a) for a in reads]
+        reads=[a+"," for a in reads]
+        muts=[str(mut_idx)]*mut_reads
+        muts=[a+"," for a in muts]
+        read_df=pd.DataFrame(dict(reads=reads,muts=muts))
+        reads_df=reads_df.append(read_df,ignore_index=True)
 
+    seq_pos_df=pd.DataFrame(dict(seq_pos=seq_pos,mut_indices=mut_indices))
+    seq_pos_df=seq_pos_df.merge(errors[["errortype","mut_indices","length","alt"]],on="mut_indices")
 
+    reads_df=reads_df.groupby("reads").sum()
+    reads_df["count"]=1
+    reads_df=reads_df.groupby("muts",as_index=False).sum()
+    reads_df["muts"]=reads_df.apply(lambda x: x.muts.split(",")[:-1] ,axis=1)
 
+    amplicons.append(i.amplicon_filepath)
+    n_reads.append(i.n_reads - sum(reads_df["count"]))
 
+    for idx,pcr_error in enumerate(reads_df.itertuples()):
+        final_df=seq_pos_df.loc[seq_pos_df["mut_indices"].isin([int(a) for a in pcr_error.muts]),]
+        final_df=final_df.sort_values('seq_pos')
+        
+        new_seq=""
+        for indx,final in enumerate(final_df.itertuples()):
+            if indx==0:
+                new_seq=new_seq + seq[0:final.seq_pos]
+            if final.errortype=="SUBS" or final.errortype=="INS":
+                new_seq=new_seq+final.alt
+                try:
+                    new_seq=new_seq+ seq[final.seq_pos+1:final_df.loc[indx+1,"seq_pos"]]
+                except KeyError:
+                    new_seq=new_seq+ seq[final.seq_pos+1:]
+            elif final.errortype=="DEL":
+                try:
+                    new_seq=new_seq+ seq[final.seq_pos+1:final_df.loc[indx+1,"seq_pos"]][final.length-1:]
+                except KeyError:
+                    new_seq=new_seq+ seq[final.seq_pos+1:][final.length-1:]
 
+        new_path=i.amplicon_filepath[:-6] + "_p" +str(idx+1) + ".fasta"
+        amplicons.append(new_path)
+        n_reads.append(pcr_error.count)
 
-
-
-
-
-
-
-lens=[]
-for genome in SeqIO.parse("tmp.sms.hCoV-19&Scotland&QEUH-15AFBCE&2021|EPI_ISL_2354066|2021-05-22_amplicon_40.1.fq", format="fastq"):
-    gen1=genome
-    break
-
-
-seq1=genome.seq
-seq2
-set(lens)
-
-seq_list=list(genome.seq)
-seq_list[0:10]
-seq_list[0]="A"
-genome.seq=Seq("".join((seq_list)))
-seq3=list(seq1)
-seq3=seq3[:50]+seq3[53:]
-seq3=Seq("".join(seq3))
-seq4=list(seq1)
-seq4=seq4[:50]+["A","C","G"]+seq4[50:]
-seq4=Seq("".join(seq4))
-
-ref=REF.seq
-alignments = pairwise2.align.localms(seq1, seq2.reverse_complement(),10, -10, -3, -0.5)
-print(format_alignment(*alignments[0],full_sequences=True))
-alignments[0:5]
-
-
-
-align1=pairwise2.align.localms(ref,seq1,5, -2, -3, -0.5)
-align2=pairwise2.align.localms(ref, seq2.reverse_complement(),10, -10, -10, -0.5)
-align3=pairwise2.align.localms(ref, seq3,5, -2, -3, -0.5)
-align4=pairwise2.align.localms(ref, seq4,5, -1, -3, -0.5)
-print(format_alignment(*align1[0]))
-print(format_alignment(*align2[0]))
-print(format_alignment(*align3[0]))
-print(format_alignment(*align4[0]))
-align1[0].start
-align2[0].start
-ref[12060:12065]
-seq1[0:5]
-
-
-align3[0].seqB[align3[0].start:align3[0].end]
-align4[0].seqB[align4[0].start:align4[0].end]
-align4[0].seqA[align4[0].start:align4[0].end]
-
-os.getcwd()
+        with open(f"../example/amplicons/{new_path}","w") as new_a:
+            new_a.write(f">{new_path[:-6]}\n")
+            new_a.write(new_seq + "\n\n")
+            
